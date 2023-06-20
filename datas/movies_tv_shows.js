4ac1ee69fd0c49f7ae22dd9e2a09cece2657a310
saqib <saqib29111@gmail.com>
2023-06-21 00:47:53 +0600

    updated movies_TVShows model

--- datas/movies_tv_shows.js
+++ datas/movies_tv_shows.js
@@ -1,97 +1,125 @@
 const movies_tvShows = [
-    {
-        "title": "The Avengers",
-        "runtime": 143,
-        "actors": [
-          {
-            "name": "Robert Downey Jr."
-          },
-          {
-            "name": "Chris Evans"
-          },
-          {
-            "name": "Mark Ruffalo"
-          },
-          {
-            "name": "Chris Hemsworth"
-          },
-          {
-            "name": "Scarlett Johansson"
-          }
-        ],
-        "directors": [
-          {
-            "name": "Joss Whedon"
-          }
-        ],
-        "producers": [
-          {
-            "name": "Kevin Feige"
-          }
-        ],
+  {
+    "title": "Inception",
+    "runtime": 148,
+    "type": "movie",
+    "genre": "Science Fiction",
+    "actors": [
+      {
+        "name": "Leonardo DiCaprio"
       },
       {
-        "title": "Inception",
-        "runtime": 148,
-        "actors": [
-          {
-            "name": "Leonardo DiCaprio"
-          },
-          {
-            "name": "Joseph Gordon-Levitt"
-          },
-          {
-            "name": "Ellen Page"
-          },
-          {
-            "name": "Tom Hardy"
-          },
-          {
-            "name": "Ken Watanabe"
-          }
-        ],
-        "directors": [
-          {
-            "name": "Christopher Nolan"
-          }
-        ],
-        "producers": [
-          {
-            "name": "Emma Thomas"
-          },
-          {
-            "name": "Christopher Nolan"
-          }
-        ],
+        "name": "Joseph Gordon-Levitt"
       },
       {
-        "title": "The Shawshank Redemption",
-        "runtime": 142,
-        "actors": [
-          {
-            "name": "Tim Robbins"
-          },
-          {
-            "name": "Morgan Freeman"
-          },
-          {
-            "name": "Bob Gunton"
-          },
-          {
-            "name": "William Sadler"
-          }
-        ],
-        "directors": [
-          {
-            "name": "Frank Darabont"
-          }
-        ],
-        "producers": [
-          {
-            "name": "Niki Marvin"
-          }
-        ],
+        "name": "Ellen Page"
       }
+    ],
+    "directors": [
+      {
+        "name": "Christopher Nolan"
+      }
+    ],
+    "producers": [
+      {
+        "name": "Emma Thomas"
+      },
+      {
+        "name": "Christopher Nolan"
+      }
+    ],
+  },
+  {
+    "title": "Stranger Things",
+    "runtime": 60,
+    "type": "tv show",
+    "genre": "Science Fiction, Horror, Drama",
+    "actors": [
+      {
+        "name": "Millie Bobby Brown"
+      },
+      {
+        "name": "Finn Wolfhard"
+      },
+      {
+        "name": "David Harbour"
+      }
+    ],
+    "directors": [
+      {
+        "name": "The Duffer Brothers"
+      }
+    ],
+    "producers": [
+      {
+        "name": "Shawn Levy"
+      },
+      {
+        "name": "Dan Cohen"
+      }
+    ],
+  },
+  {
+    "title": "The Dark Knight",
+    "runtime": 152,
+    "type": "movie",
+    "genre": "Action, Crime, Drama",
+    "actors": [
+      {
+        "name": "Christian Bale"
+      },
+      {
+        "name": "Heath Ledger"
+      },
+      {
+        "name": "Michael Caine"
+      }
+    ],
+    "directors": [
+      {
+        "name": "Christopher Nolan"
+      }
+    ],
+    "producers": [
+      {
+        "name": "Emma Thomas"
+      },
+      {
+        "name": "Christopher Nolan"
+      }
+    ],
+  },
+  {
+    "title": "Friends",
+    "runtime": 30,
+    "type": "tv show",
+    "genre": "Comedy, Romance",
+    "actors": [
+      {
+        "name": "Jennifer Aniston"
+      },
+      {
+        "name": "Courteney Cox"
+      },
+      {
+        "name": "Matthew Perry"
+      }
+    ],
+    "directors": [
+      {
+        "name": "Various"
+      }
+    ],
+    "producers": [
+      {
+        "name": "Kevin S. Bright"
+      },
+      {
+        "name": "Marta Kauffman"
+      }
+    ],
+  },  
+  
 ]
 
 export default movies_tvShows
