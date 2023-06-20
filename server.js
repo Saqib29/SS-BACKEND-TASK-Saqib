52a38ab9f980c6e7156a9f66d9fbf4db5e140659
saqib <saqib29111@gmail.com>
2023-06-20 18:28:01 +0600

    add custom error handler

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import moviesTVShwosRoute from './routes/movies_tvshowsRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const app = express()
 
 dotenv.config()
@@ -14,6 +15,9 @@ app.use(express.json())
 app.use('/api/movies-and-tv-shows', moviesTVShwosRoute)
 app.use('/api/users', userRoutes)
 
+// Custome error handler
+app.use(notFound)
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.send('API is running...')
