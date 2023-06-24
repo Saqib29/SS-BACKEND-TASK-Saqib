09a1a892a3755d23cc605c1df60a7bf62869f979
saqib <saqib29111@gmail.com>
2023-06-24 22:08:59 +0600

    update db_name

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URL, {
+            dbName: process.env.DB_NAME,
             useNewUrlParser: true,
             useUnifiedTopology: true,
           })
